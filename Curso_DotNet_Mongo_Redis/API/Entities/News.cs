ca39922fd3701bd99fa41f55542f85555972c56f
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2022-12-12 19:26:15 -0300

    Criação inicial do projeto e implementação das classes BaseEntity e News

---
+++ Curso_DotNet_Mongo_Redis/API/Entities/News.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace API.Entities
+{
+    public class News : BaseEntity
+    {
+        [BsonElement("hat")]
+        public string Hat { get; private set; }
+
+        [BsonElement("title")]
+        public string Title { get; private set; }
+
+        [BsonElement("text")]
+        public string Text { get; private set; }
+
+        [BsonElement("author")]
+        public string Author { get; private set; }
+
+        [BsonElement("img")]
+        public string Img { get; private set; }
+
+        [BsonElement("link")]
+        public string Link { get; private set; }
+
+        [BsonElement("publishDate")]
+        public DateTime PublishDate { get; private set; }
+
+        [BsonElement("active")]
+        public bool Active { get; private set; }
+
+        public News(string hat, string title, string text, string author, string img, string link, DateTime publishDate, bool active)
+        {
+            Hat = hat;
+            Title = title;
+            Text = text;
+            Author = author;
+            Img = img;
+            Link = link;
+            PublishDate = DateTime.Now;
+            Active = active;
+        }
+    }
+}
