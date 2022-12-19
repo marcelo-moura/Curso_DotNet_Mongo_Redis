a5d36d1f36a63fc062cfb5566b76db5a2dbd13a1
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2022-12-19 19:04:08 -0300

    Adicionado teste para o metodo GenerateSlug

---
+++ Curso_DotNet_Mongo_Redis/API.Tests/Core/UtilsTests.cs
@@ -0,0 +1,21 @@
+﻿using API.Core;
+using Xunit;
+
+namespace API.Tests.Infra
+{
+    public class UtilsTests
+    {
+        [Fact]
+        public void Should_Return_Validate_Slug()
+        {
+            //Arrange
+            var title = "Fim de ano da Band traz programas especiais, filmes e shows exclusivos";
+
+            //Act
+            var slug = Utils.GenerateSlug(title);
+
+            //Assert
+            Assert.Equal("fim-de-ano-da-band-traz-programas-especiais-filmes-e-shows-exclusivos", slug);
+        }
+    }
+}
